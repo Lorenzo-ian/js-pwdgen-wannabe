25504d0b3804a8fbf7b09720546ede86900baece
Lorenzo-ian <lorenzoiannini96@gmail.com>
2019-07-01 02:49:47 +0200

    Creatore di password (insicura) completato.

---
+++ master.js
@@ -0,0 +1,5 @@
+var nome = prompt ("Inserisci il tuo nome"); //Variabile nome
+var cognome = prompt ("Inserisci il tuo cognome"); //Variabile cognome
+var colore = prompt ("Inserisci il tuo colore preferito"); // Variabile colore preferito
+
+document.body.innerHTML = nome + cognome + colore + "19"; //Variabile con password creata
